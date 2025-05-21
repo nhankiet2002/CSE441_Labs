c0c05f6288725363022b455e5015c490374f4b0f
nhankiet2002 <nhankiet2002@gmail.com>
2025-05-21 17:06:52 +0700

    12.3

--- Lab12/12.3/app/src/main/java/com/example/spinner/MainActivity.java
+++ Lab12/12.3/app/src/main/java/com/example/spinner/MainActivity.java
@@ -1,24 +1,53 @@
 package com.example.spinner;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String arrCategories[] = {"Hàng Điện tử", "Hàng Hóa Chất",
+            "Hàng Gia dụng", "Hàng xây dựng"};
+    TextView txtSelection;
+    Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txtSelection = (TextView) findViewById(R.id.txtSelection);
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+
+
+        ArrayAdapter<String> adapterCategories = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.simple_spinner_item,
+                arrCategories
+        );
+
+        adapterCategories.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1.setAdapter(adapterCategories);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // TODO Auto-generated method stub
+                txtSelection.setText(arrCategories[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
         });
     }
 }
