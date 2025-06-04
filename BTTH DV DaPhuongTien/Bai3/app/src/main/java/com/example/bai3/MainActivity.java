db6b8381a0a3927064099cf78791febb0d0cdd3b
nhankiet2002 <nhankiet2002@gmail.com>
2025-06-04 16:22:42 +0700

    3

--- BTTH DV DaPhuongTien/Bai3/app/src/main/java/com/example/bai3/MainActivity.java
+++ BTTH DV DaPhuongTien/Bai3/app/src/main/java/com/example/bai3/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.bai3;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private VideoView videoView;
+    private static final int REQUEST_VIDEO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        videoView = findViewById(R.id.videoView);
+        Button btnSelect = findViewById(R.id.btnSelect);
+
+        btnSelect.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, REQUEST_VIDEO);
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_VIDEO && resultCode == RESULT_OK && data != null) {
+            Uri videoUri = data.getData();
+            videoView.setVideoURI(videoUri);
+            videoView.setMediaController(new MediaController(this));
+            videoView.start();
+        }
     }
 }
